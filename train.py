5d472b51e44258b4367d97b971be906dba6b36a8
Patryk <patryk.niemczyk12345@gmail.com>
2025-05-10 20:02:40 +0200

    feat: add loss function and AdamW optimizer

---
+++ train.py
@@ -0,0 +1,35 @@
+from autograd import Variable
+
+def lossfn(output, target):
+	loss = Variable(0.0)
+	for o, t in zip(output, target):
+		loss += (o - t) ** 2
+	return loss / len(output)
+
+class AdamW:
+	
+	def __init__(self, parameters, lr=0.001, weight_decay=0.01, beta1=0.9, beta2=0.95, eps=1e-8):
+		self.parameters = parameters
+		self.lr = lr
+		self.weight_decay = weight_decay
+		self.beta1 = beta1
+		self.beta2 = beta2
+		self.eps = eps
+		self.m = [Variable(0.0) for _ in parameters]
+		self.v = [Variable(0.0) for _ in parameters]
+		self.t = 0
+  
+	def step(self):
+		self.t += 1
+		for i, param in enumerate(self.parameters):
+			if param.grad is not None:
+				self.m[i] = self.beta1 * self.m[i] + (1 - self.beta1) * param.grad
+				self.v[i] = self.beta2 * self.v[i] + (1 - self.beta2) * (param.grad ** 2)
+				m_hat = self.m[i] / (1 - self.beta1 ** self.t)
+				v_hat = self.v[i] / (1 - self.beta2 ** self.t)
+				param.data -= self.lr * m_hat / (v_hat ** 0.5 + self.eps)
+				param.data -= self.weight_decay * param.data
+    
+	def zero_grad(self):
+		for param in self.parameters:
+			param.grad = 0.0
